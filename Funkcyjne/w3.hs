fc79bad02f9c766b21ffb7b61c7187a6a5c13e1f
Jakub Kowal <ikubutek@gmail.com>
2025-03-18 14:58:29 +0100

    Skonczona notatka z wykladu w3

--- Funkcyjne/w3.hs
+++ Funkcyjne/w3.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 module W03 where
 
 -- import Data.List {transpose, partition, permutations}
@@ -22,12 +24,31 @@ qSort [x] = [x]
 qSort (x:xs) = (qSort l) ++ [x] ++ (qSort r)
                 where (l,r) = partition (<x) xs
 -- inSort
+inSort [] = []
+inSort (x:xs) = l ++ [x] ++ r
+                where sxs = inSort xs
+                      (l,r) = partition (<x) sxs
 -- mergeSort
 
 
 -- zip
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y): (zip' xs ys)
+
 -- zipWith
 
 
 -- myfoldr
+myfoldr op e [] = e
+myfoldr op e (x:xs) = op x (myfoldr op e xs)
+
 -- myfoldl
+myfoldl op e [] = e
+myfoldl op e (x:xs) = myfoldl op (op e x) xs
+
+add [] = 0
+add (x:xs) = x + add xs
+
+pro [] = 1
+pro (x:xs) = x * pro xs
