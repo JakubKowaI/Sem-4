a91f054479adfd9863c15e82d006a16f84bc9783
Jakub Kowal <ikubutek@gmail.com>
2025-04-02 02:13:34 +0200

    Dodanie do L2 algo

--- mw1/L2/Wykres.py
+++ mw1/L2/Wykres.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("resultsDG.csv")
+df = pd.read_csv("resultsDPIG.csv")
 
 plt.figure(figsize=(10, 5))
 sns.barplot(x="Array Size", y="Comparisons", hue="Algorithm", data=df)
@@ -11,7 +11,7 @@ plt.ylabel("Comparisons")
 plt.xlabel("Array Size")
 plt.legend(title="Algorithm")
 plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.savefig("ComparisonsDG.png", dpi=300, bbox_inches="tight")
+plt.savefig("ComparisonsDPIG.png", dpi=300, bbox_inches="tight")
 
 plt.figure(figsize=(10, 5))
 sns.barplot(x="Array Size", y="Swaps", hue="Algorithm", data=df)
@@ -20,4 +20,4 @@ plt.ylabel("Swaps")
 plt.xlabel("Array Size")
 plt.legend(title="Algorithm")
 plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.savefig("SwapsDG.png", dpi=300, bbox_inches="tight")
+plt.savefig("SwapsDPIG.png", dpi=300, bbox_inches="tight")
