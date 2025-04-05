78603ffff83fdb4155f631309324a4341b21af8f
Jakub Kowal <ikubutek@gmail.com>
2025-04-05 21:54:51 +0200

    Done L2 algorytmy

--- mw1/L2/WykresZ2.py
+++ mw1/L2/WykresZ2.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
-k=100
+k=1
 
 # Load the data from CSV
-#df = pd.read_csv("resultsQSK"+str(k)+".csv")
-df = pd.read_csv("plots/Z3/results/results.csv")
+df = pd.read_csv("plots/HighN/results/results50K"+str(k)+".csv")
+#df = pd.read_csv("plots/QS/results/results.csv")
 
 
 # Ensure correct data types
@@ -29,10 +29,15 @@ def plot_metric(metric, title, ylabel, filename):
 
     # Compute x log2(x) for the given x values
     x_values = np.array(sorted(df_avg["Array Size"].unique()))
-    y_values = 0.6*x_values * np.log2(x_values)
+    y_values = 0.65*x_values * np.log2(x_values)
+    #y_values = 0.25*(x_values*x_values)
+
+    #y_values2= 0.55*x_values * np.log2(x_values)
 
     # Plot x log2(x) as a reference
-    plt.plot(x_values, y_values, linestyle="--", color="black", label="0,6*x log2(x)")
+    plt.plot(x_values, y_values, linestyle="--", color="black", label="0,65*nlog2(n)")
+
+    #plt.plot(x_values, y_values2, linestyle="--", color="black", label="0,55*nlog2(n)")
 
     plt.title(title)
     plt.xlabel("Array Size (n)")
@@ -71,11 +76,11 @@ def plot_metric2(metric, title, ylabel, filename):
     plt.close()
 
 # Generate and save the required plots
-plot_metric2("Comparisons", "Average Number of Comparisons vs Array Size", "Average Comparisons", "plots/Z3/comparisons_avg"+str(k)+".png")
-plot_metric2("Swaps", "Average Number of Swaps vs Array Size", "Average Swaps", "plots/Z3/swaps_avg"+str(k)+".png")
+#plot_metric("Comparisons", "Average Number of Comparisons vs Array Size", "Average Comparisons", "plots/HighN/comparisons_avg50K"+str(k)+".png")
+plot_metric("Swaps", "Average Number of Swaps vs Array Size", "Average Swaps", "plots/HighN/swaps_avg50K"+str(k)+".png")
 df_avg["Comparisons per n"] = df_avg["Comparisons"] / df_avg["Array Size"]
 df_avg["Swaps per n"] = df_avg["Swaps"] / df_avg["Array Size"]
-plot_metric2("Comparisons per n", "Comparisons per n", "Comparisons / n", "plots/Z3/comparisons_per_n"+str(k)+".png")
-plot_metric2("Swaps per n", "Swaps per n", "Swaps / n", "plots/Z3/swaps_per_n"+str(k)+".png")
+plot_metric2("Comparisons per n", "Comparisons per n", "Comparisons / n", "plots/HighN/comparisons_per_n50K"+str(k)+".png")
+plot_metric2("Swaps per n", "Swaps per n", "Swaps / n", "plots/HighN/swaps_per_n50K"+str(k)+".png")
 
 print("Plots saved: comparisons_avg.png, swaps_avg.png, comparisons_per_n.png, swaps_per_n.png")
